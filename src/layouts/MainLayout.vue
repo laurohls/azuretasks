61c80c061a02736d65d780f22e2b7c0569421621
laurohls <laurohls@gmail.com>
2023-02-01 15:28:29 -0400

    mainManu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Azure Tasks </q-toolbar-title>
+        <q-toolbar-title> Tarefas do Azure </q-toolbar-title>
 
         <div>PSG Tecnologia</div>
       </q-toolbar>
